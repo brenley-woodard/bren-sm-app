61acc881fe147d18d4798a5c8992ac1e78c614d5
brenley-woodard <brenleyew@gmail.com>
2022-12-08 14:25:27 -0800

    render message function complete

--- render-utils.js
+++ render-utils.js
@@ -20,8 +20,44 @@ export function renderProfile(profileObject) {
 
 export function renderMessages(profile) {
     const ul = document.createElement('ul');
+    const header = document.createElement('h3');
+
+    header.textContent = `Message Feed for ${profile.username}`;
+
     ul.classList.add('messages');
 
-    const header = document.createElement('h3');
+    ul.append(header);
+
+    for (let i = 0; i < profile.messages.length; i++) {
+        console.log('i', profile.messages[i]);
+
     const li = document.createElement('li');
+    li.classList.add('message');
+
+    const div = document.createElement('div');
+    div.classList.add('message-info');
+
+    const senderSpan = document.createElement('span');
+    senderSpan.classList.add('from');
+    senderSpan.textContent = profile.messages[i].from_user;
+
+    const dateSpan = document.createElement('span');
+    dateSpan.classList.add('created-date');
+    dateSpan.textContent = new Date(profile.messages[i].created_at).toLocaleString('en-US', {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    });
+
+    const text = document.createElement('p');
+    text.classList.add('text');
+    text.textContent = profile.messages[i].text;
+
+    div.append(senderSpan, dateSpan);
+
+    li.append(div, text);
+
+    ul.append(li);
 }
